d41943bce5f3700adacb1daa66853b93ca1766db
Ronny Badilla <info@ronnybadilla.com>
2025-05-13 22:33:49 -0600

    docs: env variables

--- app/(docs)/docs/layout.config.tsx
+++ app/(docs)/docs/layout.config.tsx
@@ -1,6 +1,6 @@
 import { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-import Logo from '@/components/logos';
 import { GithubInfo } from 'fumadocs-ui/components/github-info';
+
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: 'zeta',
