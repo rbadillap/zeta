ba78a7073fb68eeaf8ac632855a9caf2f1f5c00e
Ronny Badilla <info@ronnybadilla.com>
2025-05-13 21:32:45 -0600

    docs: welcome

--- app/(docs)/docs/layout.config.tsx
+++ app/(docs)/docs/layout.config.tsx
@@ -1,7 +1,17 @@
 import { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
+import Logo from '@/components/logos';
+import { GithubInfo } from 'fumadocs-ui/components/github-info';
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: 'zeta',
   },
+  links: [
+    {
+      type: 'custom',
+      children: (
+        <GithubInfo owner="rbadillap" repo="zeta" />
+      ),
+    },
+  ],
+
 };
--- lib/source.ts
+++ lib/source.ts
@@ -1,8 +1,20 @@
 // .source folder will be generated when you run `next dev`
 import { docs } from '@/.source';
+import { createElement } from 'react';
+import { icons } from 'lucide-react';
 import { loader } from 'fumadocs-core/source';
+import Logo, { brands } from '@/components/logos';
 
 export const source = loader({
   baseUrl: '/docs',
+  icon(icon) {
+    if (icon && icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
+    }
+
+    if (icon && brands.includes(icon)) {
+      return createElement(Logo, { name: icon });
+    }
+  },
   source: docs.toFumadocsSource(),
 });
